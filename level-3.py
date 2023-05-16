4b20cc07cb3409569ee914669600ba67ada982f8
Leo Wilkin <leo.wilkin10@gmail.com>
2023-05-16 17:47:09 +0100

    Create level-3.py

---
+++ level-3.py
@@ -0,0 +1,59 @@
+# Challenge 34
+# 10 * 7 == 70
+# True
+# Challenge 35
+# "John" != "JOHN"
+# True
+# Challenge 36
+# 2*4 and 28 <= 7*4
+# True
+# Challenge 37
+# x and y < 0
+# Challenge 38
+# a or b == 0
+# Challenge 39
+# a = 24/8
+# b = 24+8
+# print(a==b)
+# False
+# Challenge 40
+# 237//17 == 237/17
+# False
+# Challenge 41
+# print((10==5*2) and (45!=9*5))
+# False
+# Challenge 42
+# print((6*3>=9*2) or (30//7==2))
+# True
+# Challenge 43
+#lhWeight = input("Please enter #left hand weight. ")
+#rhWeight = input("Please enter #right hand weight. ")
+#if lhWeight == rhWeight:
+#  print("The rollercoaster is #balanced.")
+#else:
+#  print("The rollercoaster is #NOT balanced.")
+# Challenge 44
+#pressure = int(input("Input tyre #pressure in PSI: "))
+#if pressure >= 28 and pressure ##<= 32:
+#  print("Tyre pressure OK")
+#else:
+#  print("Tyre pressure too high #or too low.")
+# Challenge 45
+#width = input("Input width = ")
+#length = input("Input length = ")
+#if width == length:
+#  print("The maze is square.")
+#else:
+#  print("The maze is rectangular.")
+# Challenge 46
+# Needs file
+# Challenge 47
+#twentyRest = input("Have you #been at rest for at least 20 #minutes? (Enter Y or N): ")
+#if twentyRest == "Y" or "y":
+#  pulseRate = int(input("Is your #pulse rate between 60 and 100 #bpm? (Enter Y or N): "))
+#  if int(pulseRate) >= int(60) #and int(pulseRate) <= int(100):
+#    print("Your heart rate is #within the expected range.")
+#  else:
+#    print("Your resting heart #rate is outside of the expected #range. You may wish to consult a #health professional.")
+#else:
+#  print("Rest for 20 minutes and #try again!")
