21577bed1de42278e62d9afef7e8c81a9e8f1cd7
Leo Wilkin <leo.wilkin10@gmail.com>
2023-05-16 17:47:55 +0100

    Create level-2.py

---
+++ level-2.py
@@ -0,0 +1,112 @@
+# Challenge 17
+#books = 650
+#children = 173
+#booksPerChild = books // children
+#booksLeftOver = books % children
+#print("Books per child", booksPerChild)
+#print("Books left over", booksLeftOver)
+
+# Challenge 18
+#n1 = 13685
+#n2 = 27
+#print(n1, "divided by" , n2, "is", int(n1 // n2), "remainder", int(n1 % n2))
+
+# Challenge 19
+# stepsMonday = float(23.5)
+# stepsTuesday = float(15.4)
+# print(float(stepsMonday) + float(stepsTuesday)) 
+
+# Challenge 20
+# firstDivision = 7 * 3
+# secondDivision = 16 / 5
+# print(int(firstDivision) + int(secondDivision))
+
+# Challenge 21
+# print(int(2) ** int(10))
+
+# Challenge 22
+# cabFare = 4.50
+# flight = 91.25
+# train = 11.80
+# total = cabFare + flight + train
+# print("Total travel expenses = £",total)
+
+# Challenge 23
+# cabFare = input("Please enter your Cab fare: ")
+# flight = input("Please enter your Flight fare: ")
+# train = input("Please enter your Train fare: ")
+# total = float(cabFare) + float(flight) + float(train)
+# print("Total travel expenses = £",total)
+
+# Challenge 24
+# string
+# integer
+# float
+
+# Challenge 25
+# weightGoods = input("What is your current load of goods? ")
+# totalWeight = int(weightGoods) + int(24350)
+# print("The total weight of your truck is", totalWeight)
+# if (totalWeight >= 30000):
+#  print("Your weight limit is over national standards for  # bridge crossings. Check with county requirements.")
+# else:
+#  print("Have a good trip.")
+
+# Challenge 26
+# basicFee = int(4)
+# print(basicFee)
+# feePerKilometer = float(0.80)
+# print(feePerKilometer)
+# kilometersTraveled = input("How far have you travelled from #the hotel to the airport? ")
+# priceOfDistance = float(feePerKilometer) * # float(kilometersTraveled)
+# print(priceOfDistance)
+# print("The total fee is ", float(priceOfDistance) + # int(basicFee))
+
+# Challenge 27
+# F = input("Enter farenheit temperature: ")
+# C = (float(F) - 32) * 5/9
+# print(C)
+
+# Challenge 28
+# num1 = int(input("Enter an integer: "))
+# num2 = int(input("Enter another integer: "))
+# sum = num1 + num2
+# print("The sum of " + str(num1) + " and " + str(num2) + " is " + str(sum))
+
+# Challenge 29
+#pupilName = input("What is the student's name? ")
+#mark = input("Input mark scored out of 20 by " + pupilName + " : ")
+#percentage = int(mark) * int(5)
+#print(pupilName + " scored " + str(percentage) + " percent.")
+
+# Challenge 30
+#print("Instructions: Enter two strings and have them swapped around. #Simples.")
+#x = input("First string: ")
+#y = input("Second string: ")
+#holder = x
+#x = y
+#y = holder
+#print(x, "and", y)
+
+# Challenge 31
+# Skipped for lack of Challenge folder
+
+# Challenge 32
+#print("Welcome to NASA Kennedy Centre Flight Control Command, Lt. Corp. Fieldmaster General Lord Jedi Aldrin")
+#numberHours = input("Please input number of hours until launch: ")
+#if int(numberHours) <= int(24):
+#  print("Launch in " + str(numberHours) + " hours.")
+#else :
+#  days = int(numberHours) // 24
+#  hours = int(numberHours) % 24
+#  if int(days) == 1:
+#    print("Launch in " + str(days) + " day,")
+#  else :
+#    print("Launch in " + str(days) + " days, ")
+#  if int(hours) == 1:
+#    print(str(hours) + " hour.")
+#  else:
+#    print(str(hours) + " hours.")
+
+# Challenge 33
+# The program will pring "num 20"
