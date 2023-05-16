c56914a562d836cc77f58b0ca86b459d9af51014
Leo Wilkin <leo.wilkin10@gmail.com>
2023-05-16 17:44:54 +0100

    Create level-5.py

---
+++ level-5.py
@@ -0,0 +1,100 @@
+# Challenge 71
+#planet = ["Mercury","Venus","Earth","Mars", "Jupiter","Saturn" ]
+#n = len(planet)
+#for i in range(n):
+#  print(planet[i])
+
+# Challenge 72
+#planet = ["Mercury","Venus","Earth","Mars", "Jupiter","Saturn" ]
+#planet.append("Uranus")
+#planet.append("Neptune")
+#n = len(planet)
+#for i in range(n):
+#  print(planet[i])
+
+# Challenge 73
+#planet = ["Mercury", "Venus", "Mars", #"Jupiter", "Saturn"]
+#planet.insert(2, "Earth")
+#print(planet)
+
+# Challenge 74
+#planet = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", #"Saturn"]
+#for index in range(5, -1, -1):
+#  print(planet[index])
+
+# Challenge 75
+#astronauts = list()
+#years = ["2016", "2017", "2018", "2019", "2020"]
+#for i in range(0,5,1):
+#  astros = input("How many astronauts on space missions in #"+years[i]+": ")
+  #astronauts.append(astros)
+#print(astronauts)
+
+# Challenge 76
+#totals = [0]*100
+#print(totals)
+#n = len(totals)
+#print(n)
+
+# Challenge 77
+# Folder N/A
+
+# Challenge 78
+#planets = ["jupiter", "saturn", "uranus", "neptune", "venus", #"mars", "mercury", "earth"]
+#sizes = ["1120%", "945%", "400%", "390%", "95%", "53%", "38%", #"100%"]
+#for i in range(0, 8, 1):
+#  print(planets[i],sizes[i],"the size of Earth")
+
+# Challenge 79
+# Folder
+
+# Challenge 80 & 81
+#planets = ["jupiter", "saturn", "uranus", "neptune", "venus", "mars", "mercury", "earth"]
+#sizes = ["1120%", "945%", "400%", "390%", "95%", "53%", "38%", "100%"]
+#for i in range(0, 8, 1):
+#  print(planets[i],sizes[i],"the size of Earth")
+#sizes.append("388%")
+#print(planets[3], sizes[8])
+
+# Challenge 82
+#booking = [[57,68,100,124], [43,52,92,101], [72,78,84,90]]
+#booking[1][2] = 96
+#print(booking)
+
+# Challenge 83
+#siteNum = 0
+#monthNum = 0
+#
+#def monthfunc(month):
+#  if month == "may":
+#    return 0
+#  elif month == "june":
+#    return 1
+#  elif month == "july":
+#    return 2
+#  elif month == "august":
+#    return 3
+#  else:
+#    return -1
+#
+#booking = [[57, 68, 100, 124], [43, 52, 96, 101], [72, 78, 84, 90]]
+#
+#site = input("What site do you want to view bookings for: (Cambridge/Newcastle/Manchester) ")
+#
+#if site == "cambridge":
+#  siteNum = 0
+#elif site == "newcastle":
+#  siteNum = 1
+#elif site == "manchester":
+#  siteNum = 2
+#
+#month = input("What month do you want to view bookings for: (May, June, July, August) ")
+#monthNum = monthfunc(month)
+#
+#if siteNum >= 0 and monthNum >= 0:
+#  print("Bookings for " + site + " in " + month + " was " + str(booking[siteNum][monthNum]))
+#else:
+#  print("Invalid site or month entered.")
+
+# Challenge 84
+# Challenge yourself, DIY
