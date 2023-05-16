c1d4c26818adf3a41c1a184a6c06cf773e9df3b2
Leo Wilkin <leo.wilkin10@gmail.com>
2023-05-16 17:48:35 +0100

    Create level-1.py

---
+++ level-1.py
@@ -0,0 +1,61 @@
+# print("Gorillas are the largest living primates")
+
+# print("The scientific name for the Western Lowland \ gorilla is 'gorilla gorilla gorilla'")
+
+# print("don't worry \n be happy")
+
+# print("Hi there! \n")
+# firstname = input("What is your name? ")
+# print("Hello", firstname)
+
+# error is lack of closing speech mark in line 2
+
+# animal1 = input("Name one animal")
+# animal2 = input("Name one animal")
+# animal3 = input("Name one animal")
+# print(animal1, animal2, animal3)
+
+# print("one variable in the last challenge was animal1")
+
+# Correct the syntax errors in this program
+# print("This program contains syntax errors \n")
+# favouriteAnimal = "Giraffe"
+# scariestAnima1 = "Lion"
+# print("Favourite animal", favouriteAnimal, "\n"
+# "Scariest animal.... " , scariestAnima1 )
+
+# (a) is valid
+
+# when you run the code, you will get 10 5
+
+# when you run the code, you will get Fred Tom Fred
+
+# x = 5
+# y = 10
+# x = y
+# y = 5
+# print(x, y)
+# x = "Tom"
+# y = "Fred"
+# z = "Henry"
+# z = y
+# y = x
+# x = z
+# print(x, y, z)
+
+# a constant
+# b variable
+# c variable
+# d constant
+
+# firstName = input("What is your first name: ")
+# lastName = input("What is your last name: ")
+# print("Hello", lastName, firstName)
+
+# lastName = input("What is your last name: ")
+# initial = input("What is your first initial: ")
+# bloodType = input("What is your blood type: ")
+# print("Name: ", lastName, initial, "Blood Type: ", bloodType)
+
+# Missing quote mark to finish sentence
+# Typo of variable
